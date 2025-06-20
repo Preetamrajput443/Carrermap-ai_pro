8de956a59ae43f8dbada8036400e03178e03e31c
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-06-20 21:52:50 +0530

    Create tools.tsx

---
+++ pages/tools.tsx
@@ -0,0 +1,12 @@
+export default function Tools() {
+  return (
+    <div>
+      <h1>Career Tools</h1>
+      <ul>
+        <li>Resume Builder</li>
+        <li>Salary Estimator</li>
+        <li>Career Roadmap Generator</li>
+      </ul>
+    </div>
+  );
+}
