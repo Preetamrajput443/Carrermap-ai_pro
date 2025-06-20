69bc1c9f0f5de691720bd7dba34507b3fc88d4d5
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-06-20 21:54:42 +0530

    Create career-guide.tsx

---
+++ pages/career-guide.tsx
@@ -0,0 +1,8 @@
+export default function CareerGuide() {
+  return (
+    <div>
+      <h1>AI Career Guide</h1>
+      <p>Answer a few questions and get your career path mapped.</p>
+    </div>
+  );
+}
